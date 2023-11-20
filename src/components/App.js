8b0857f8d67fe4c0deb6c0c69474d18ab8ad8a57
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-20 12:32:32 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [pos, setPos] = useState('') 
+  useEffect(()=>{
+    window.addEventListener('mousemove',e=>{
+      setPos(e.clientX+","+e.clientY)
+    })
+  })
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>The mouse position is {pos}</p>
     </div>
   )
 }
